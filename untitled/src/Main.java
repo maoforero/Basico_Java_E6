00f553d7ef714919a933297660d3a2c5588c5342
maoforero <maforeror@gmail.com>
2022-11-08 00:08:00 -0500

    recorriendo una array

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,15 +1,24 @@
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Entrega 6");
         reverse("Hola Mundo");
+
+        String[] unidimensionalArray = {"Manzada", "Pera", "Uva", "Lulo"};
+
+        for (int positionArray = 0; positionArray < unidimensionalArray.length; positionArray++){
+            System.out.println(unidimensionalArray[positionArray]);
+        }
     }
 
 
     public static String reverse(String texto){
-        String textoReverse = "";
+
 
         for (int i = 0; i <  texto.length(); i++){
             System.out.println(texto.charAt(i));
+
         }
 
 
