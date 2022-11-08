7baa089dc73946bc0fdd9878fa009591790574ee
maoforero <maforeror@gmail.com>
2022-11-07 23:58:46 -0500

    Reverse function

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Entrega 6");
+        reverse("Hola Mundo");
+    }
+
+
+    public static String reverse(String texto){
+        String textoReverse = "";
+
+        for (int i = 0; i <  texto.length(); i++){
+            System.out.println(texto.charAt(i));
+        }
+
+
+        return null;
     }
 }
